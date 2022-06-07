badb2c30c961e06a9b67e258f09d05c877547945
divirdima <99564085+divirdima@users.noreply.github.com>
2022-06-07 22:28:18 +0300

    Trying to add singleton

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,28 +9,32 @@ import jm.task.core.jdbc.model.User;
 
 public class Util {
 	
+	private static Util INSTANCE;
 	private static SessionFactory sessionFactory;
 	
-	public Util() {
+	private Util() {
+		Properties prop = new Properties();
+		prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/" + "first");
+        prop.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+        prop.setProperty("hibernate.connection.username", "root");
+        prop.setProperty("hibernate.connection.password", "1111");
+        prop.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+        prop.setProperty("show_sql", "true"); //to see the generated sql query
+        prop.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        prop.setProperty("hibernate.current_session_context_class", "org.hibernate.context.internal.ThreadLocalSessionContext");
 		
-	}
+		sessionFactory = new Configuration().addAnnotatedClass(User.class).addProperties(prop).buildSessionFactory();
+    }
     
 	
-	public SessionFactory getSessionFactory() {
-		String url = "jdbc:mysql://localhost:3306/" + "first";
-		if (sessionFactory == null) {
-			Properties prop = new Properties();
-			prop.setProperty("hibernate.connection.url", url);
-	        prop.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
-	        prop.setProperty("hibernate.connection.username", "root");
-	        prop.setProperty("hibernate.connection.password", "1111");
-	        prop.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-	        prop.setProperty("show_sql", "true"); //to see the generated sql query
-	        prop.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-	        prop.setProperty("hibernate.current_session_context_class", "org.hibernate.context.internal.ThreadLocalSessionContext");
-			
-			sessionFactory = new Configuration().addAnnotatedClass(User.class).addProperties(prop).buildSessionFactory();
+	public static Util getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new Util();
 		}
+		return INSTANCE;
+	}
+	
+	public SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
 	
